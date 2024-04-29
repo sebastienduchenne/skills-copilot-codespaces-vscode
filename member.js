e5c3508f872f361e86a4cdd2ab076fc06f6b28d6
sebastienduchenne <sigmapegasus@gmail.com>
2024-04-29 16:13:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/member/member-skills.html',
+    controller: 'MemberSkillsController',
+    controllerAs: 'skills'
+  };
+}
